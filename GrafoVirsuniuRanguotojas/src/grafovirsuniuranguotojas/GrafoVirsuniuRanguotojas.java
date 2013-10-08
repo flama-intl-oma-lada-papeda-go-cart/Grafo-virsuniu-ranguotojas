de023c8f3b8bcc7ddd319d10446579f5fc8f047f
Paulius Šukys <paulius.sukys@ktu.lt>
2013-10-08 18:55:31 +0300

    Pridėtas pagrindinės klasės failas

---
+++ GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/GrafoVirsuniuRanguotojas.java
@@ -0,0 +1,16 @@
+package grafovirsuniuranguotojas;
+
+/**
+ * Pagrindinė klasė
+ * @date 2013-10-08
+ * @author Paulius Šukys
+ */
+public class GrafoVirsuniuRanguotojas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Labas!");
+    }
+}
