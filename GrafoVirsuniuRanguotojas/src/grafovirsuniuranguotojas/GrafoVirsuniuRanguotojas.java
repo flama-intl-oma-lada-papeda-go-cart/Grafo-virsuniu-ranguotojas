68f0d08264cbb20ded691fe7dda6955a0fb487e3
Paulius Šukys <paulius.sukys@ktu.lt>
2013-10-10 13:03:20 +0300

    Papildau Povilo žinutę

--- GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/GrafoVirsuniuRanguotojas.java
+++ GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/GrafoVirsuniuRanguotojas.java
@@ -11,5 +11,6 @@ public class GrafoVirsuniuRanguotojas
     {
         System.out.println("Labas!");
         System.out.println("Labas pasauli, tave sveikina Povilas!");
+        System.out.println("Paulius gavo Povilo commitą ir taip pat sveikinasi");
     }
 }
