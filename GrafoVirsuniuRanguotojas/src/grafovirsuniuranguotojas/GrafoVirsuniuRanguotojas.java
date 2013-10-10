26246e34839c2f2019d94a7cf826f475d5814209
aatsilupelis@gmail.com <AtsilupeliS@172.17.0.115>
2013-10-10 12:52:03 +0300

    sfhgfh

--- GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/GrafoVirsuniuRanguotojas.java
+++ GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/GrafoVirsuniuRanguotojas.java
@@ -9,6 +9,6 @@ public class GrafoVirsuniuRanguotojas
 {
     public static void main(String[] args) 
     {
-        System.out.println("Labas!");
+        System.out.println("Labas!gddf");
     }
 }
