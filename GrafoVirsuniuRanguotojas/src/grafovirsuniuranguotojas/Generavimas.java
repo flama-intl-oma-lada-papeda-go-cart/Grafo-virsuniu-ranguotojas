bda3548c2db77a0be9390d7702854c8f244acb6c
Povilas <Povilas@172.17.0.102>
2013-10-15 12:51:18 +0300

    Pridejau faila generavimas.java

---
+++ GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/Generavimas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grafovirsuniuranguotojas;
+
+/**
+ *
+ * @author Povilas
+ */
+public class Generavimas {
+    
+    private int[][] matrica;
+    
+        public void generuotiMatrica() {
+            
+            
+        }
+        
+        public void skaitytiMatricaIsFailo() {
+            
+            
+        }
+        
+        public void skaitytiMatricaIsEkrano() {
+            
+        }
+        
+        
+        public int[][] gautiMatrica() {
+            return matrica;
+        }
+    
+}
