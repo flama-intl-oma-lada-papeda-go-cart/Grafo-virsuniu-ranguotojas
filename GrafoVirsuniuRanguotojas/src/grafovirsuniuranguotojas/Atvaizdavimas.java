96f7cb158a406a84da63c6e699e2f9375622004c
Paulius Šukys <paulius.sukys@ktu.lt>
2013-10-15 12:09:49 +0300

    Duomenų į tekstinį failą metodo sukūrimas

--- GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/Atvaizdavimas.java
+++ GrafoVirsuniuRanguotojas/src/grafovirsuniuranguotojas/Atvaizdavimas.java
@@ -1,5 +1,10 @@
 package grafovirsuniuranguotojas;
 
+import java.util.LinkedList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Pagrindinė atvaizdavimo klasė
  * @date 2013-10-10
@@ -7,5 +12,22 @@ package grafovirsuniuranguotojas;
  */
 public class Atvaizdavimas
 {
+    LinkedList r = gautiRanguotaGrafa();
     
+    String konvertuoti_txt(String failas)
+    {
+        try
+        {
+            BufferedWriter f = new BufferedWriter(new FileWriter(failas));
+        }catch (IOException e)
+        {
+            //Klaida įrašyme!
+        }
+        return null;
+    }
+    
+    LinkedList gautiRanguotaGrafa()
+    {
+        return null;
+    }
 }
